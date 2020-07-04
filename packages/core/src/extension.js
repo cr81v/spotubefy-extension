5ebe8c49f286ae80ecb32a374b044203c04e5f20
George Bockari <bitwhys@gmail.com>
2020-07-03 19:30:49 -0700

    chore: update patch

--- packages/core/src/extension.js
+++ packages/core/src/extension.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import classNames from 'classnames'
 import {
-  Accordion,
-  AccordionItem,
-  ModalWrapper,
-  Tabs,
-  Tab,
-  CodeSnippet,
   TextInput,
+  Form,
+  FormGroup,
+  SelectItem,
+  Select,
+  TextArea,
+  Button,
 } from 'carbon-components-react'
 import { Tile } from 'carbon-components-react/lib'
 
@@ -16,19 +16,46 @@ import './styles/extension.scss'
 const Extension = props => {
   return (
     // Start with the wrapper component (Tabs)
-    <div style={{ width: '50%' }}>
-      <Tabs type="container">
-        <Tab href="#" id="tab-1" label="Tab label 1">
-          <div className="some-content">Content for first tab goes here.</div>
-        </Tab>
-        <Tab href="#" id="tab-2" label="Tab label 2">
-          <div className="some-content">Content for second tab goes here.</div>
-        </Tab>
-        <Tab href="#" id="tab-3" label="Tab label 3">
-          <div className="some-content">Content for third tab goes here.</div>
-        </Tab>
-      </Tabs>
-    </div>
+    <Tile>
+      <div>SpoTubefy</div>
+      <Form>
+        <FormGroup legendText="text-2">
+          <TextInput
+            helperText="Optional helper text here; if message is more than one line text should wrap (~100 character count maximum)"
+            id="test2"
+            invalidText="Invalid error message."
+            labelText="Text Input label"
+            placeholder="Placeholder text"
+          />
+        </FormGroup>
+        <FormGroup legendText="Text">
+          <TextArea
+            cols={50}
+            helperText="Optional helper text here; if message is more than one line text should wrap (~100 character count maximum)"
+            id="test5"
+            invalidText="Invalid error message."
+            labelText="Text Area label"
+            placeholder="Placeholder text"
+            rows={4}
+          />
+        </FormGroup>
+        <FormGroup legendText="select">
+          <Select
+            defaultValue="placeholder-item"
+            id="select-1"
+            invalidText="This is an invalid error message."
+            labelText="Select"
+          >
+            <SelectItem text="Option 1" value="option-1" />
+            <SelectItem text="Option 2" value="option-2" />
+            <SelectItem text="Option 3" value="option-3" />
+          </Select>
+        </FormGroup>
+        <Button kind="primary" tabIndex={0} type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Tile>
   )
 }
 
