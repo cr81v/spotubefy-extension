9b0c77b6e27c0d49e6594ed96904f4ff33abcdc0
George Bockari <bitwhys@gmail.com>
2020-07-03 21:46:56 -0700

    refactor: use more likely form inputs

--- packages/core/src/extension.js
+++ packages/core/src/extension.js
@@ -5,51 +5,79 @@ import {
   Form,
   FormGroup,
   SelectItem,
+  Slider,
   Select,
   TextArea,
   Button,
+  Toggle,
 } from 'carbon-components-react'
 import { Tile } from 'carbon-components-react/lib'
 
 import './styles/extension.scss'
+const helperText = 'To get started, please provide a valid YouTube playlist URI'
+
+const saveToYouTube = () => {
+  console.log('Saving playlist to your YouTube account...')
+}
 
 const Extension = props => {
   return (
     // Start with the wrapper component (Tabs)
-    <Tile>
-      <div>SpoTubefy</div>
+    <Tile className="stbfy--popup__title">
+      <div className="title">SpoTubefy</div>
       <Form>
-        <FormGroup legendText="text-2">
+        <FormGroup legendText="">
           <TextInput
-            helperText="Optional helper text here; if message is more than one line text should wrap (~100 character count maximum)"
+            helperText={helperText}
             id="test2"
             invalidText="Invalid error message."
-            labelText="Text Input label"
-            placeholder="Placeholder text"
+            labelText="Playlist URI"
+            placeholder="Enter playlist URI...."
           />
         </FormGroup>
-        <FormGroup legendText="Text">
-          <TextArea
-            cols={50}
-            helperText="Optional helper text here; if message is more than one line text should wrap (~100 character count maximum)"
-            id="test5"
-            invalidText="Invalid error message."
-            labelText="Text Area label"
-            placeholder="Placeholder text"
-            rows={4}
+        <FormGroup legendText="">
+          <Toggle
+            size="small"
+            aria-label="toggle button"
+            id="toggle-1"
+            labelText="Save to YouTube"
+            labelA="No"
+            labelB="Yes"
+            onToggle={saveToYouTube}
           />
         </FormGroup>
-        <FormGroup legendText="select">
+        <FormGroup legendText="">
           <Select
             defaultValue="placeholder-item"
             id="select-1"
             invalidText="This is an invalid error message."
-            labelText="Select"
+            labelText="Choose Playlist"
           >
-            <SelectItem text="Option 1" value="option-1" />
-            <SelectItem text="Option 2" value="option-2" />
-            <SelectItem text="Option 3" value="option-3" />
+            <SelectItem text="Playlist #1" value="option-1" />
+            <SelectItem text="Playlist #3" value="option-2" />
+            <SelectItem text="Playlist #5" value="option-3" />
           </Select>
+        </FormGroup>
+        <FormGroup legendText="">
+          <Slider
+            ariaLabelInput="Label for slider value"
+            disabled={false}
+            hideTextInput={false}
+            id="slider"
+            inputType="number"
+            labelText="Covariance"
+            light={false}
+            max={100}
+            maxLabel=""
+            min={0}
+            minLabel=""
+            name=""
+            onChange={function noRefCheck() {}}
+            onRelease={function noRefCheck() {}}
+            step={1}
+            stepMultiplier={5}
+            value={50}
+          />
         </FormGroup>
         <Button kind="primary" tabIndex={0} type="submit">
           Submit
